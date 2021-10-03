47437cf93b86ae0ed9b012b233b6a75a87867365
Filbert Mangiri <filbert.mangiri@student.umn.ac.id>
2021-10-04 04:24:09 +0700

    Revert "MVC Template"
    This reverts commit c3a80044fb8d6527186bb04a83e47b4225de36e5.

--- app/views/home/index.php
+++
@@ -1,2 +0,0 @@
-<h1>Welcome to Kampung Naga News!</h1>
-<h2>Written by <?= $data['authorName']; ?></h2>
--- app/views/templates/footer.php
+++
@@ -1,6 +0,0 @@
-
-		<script src="<?= ASSETS_URL; ?>/vendor/jquery/jquery.min.js"></script>
-		<script src="<?= ASSETS_URL; ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-		<script src="<?= ASSETS_URL; ?>/vendor/validation/jquery.validate.min.js"></script>
-	</body>
-</html>
